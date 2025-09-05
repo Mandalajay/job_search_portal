2dbdf3c97651f620acabf608f89cc0dbb6cd055c
Ajay Mandal <145788000+Mandalajay@users.noreply.github.com>
2025-09-05 19:20:01 +0200

    push frontend code

---
+++ backend/utils/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_CONN_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default connectDB;
